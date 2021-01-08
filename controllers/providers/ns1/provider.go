57b90f1194acdfb6ca0a26ea9e7c4f7f5f751274
kuritka <kuritka@gmail.com>
2021-01-08 15:40:52 +0100

    ns1 non destructive

--- controllers/providers/ns1/provider.go
+++ controllers/providers/ns1/provider.go
@@ -1,1 +1,194 @@
 package ns1
+
+import (
+	"context"
+	"encoding/json"
+	coreerrors "errors"
+	"fmt"
+	"os"
+
+	k8gbv1beta1 "github.com/AbsaOSS/k8gb/api/v1beta1"
+	"github.com/AbsaOSS/k8gb/controllers/depresolver"
+	"github.com/AbsaOSS/k8gb/controllers/internal/utils"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	types "k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	externaldns "sigs.k8s.io/external-dns/endpoint"
+
+	"sort"
+	"strings"
+)
+
+const (
+	providerName          = "NS1"
+	coreDNSExtServiceName = "k8gb-coredns-lb"
+)
+
+var log = logf.Log.WithName("controller_gslb")
+
+// TODO: move to depresolver
+var k8gbNamespace = os.Getenv("POD_NAMESPACE")
+
+type Ns1 struct {
+	gslb   *k8gbv1beta1.Gslb
+	config *depresolver.Config
+	client client.Client
+}
+
+func NewNs1(config *depresolver.Config, gslb *k8gbv1beta1.Gslb, client client.Client) (i *Ns1, err error) {
+	if gslb == nil {
+		return i, fmt.Errorf("nil *Gslb")
+	}
+	if config == nil {
+		return i, fmt.Errorf("nil *Config")
+	}
+	i = &Ns1{
+		gslb:   gslb,
+		config: config,
+		client: client,
+	}
+	return
+}
+
+func (n *Ns1) ConfigureZoneDelegation() (r *reconcile.Result, err error) {
+	ttl := externaldns.TTL(n.gslb.Spec.Strategy.DNSTtlSeconds)
+	log.Info(fmt.Sprintf("Creating/Updating DNSEndpoint CRDs for %s...", providerName))
+	var NSServerList []string
+	NSServerList = append(NSServerList, n.nsServerName())
+	NSServerList = append(NSServerList, n.nsServerNameExt()...)
+	sort.Strings(NSServerList)
+	NSServerIPs, err := n.coreDNSExposedIPs()
+	if err != nil {
+		return &reconcile.Result{}, err
+	}
+	NSRecord := &externaldns.DNSEndpoint{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        fmt.Sprintf("k8gb-ns-%s", providerName),
+			Namespace:   k8gbNamespace,
+			Annotations: map[string]string{"k8gb.absa.oss/dnstype": providerName},
+		},
+		Spec: externaldns.DNSEndpointSpec{
+			Endpoints: []*externaldns.Endpoint{
+				{
+					DNSName:    n.config.DNSZone,
+					RecordTTL:  ttl,
+					RecordType: "NS",
+					Targets:    NSServerList,
+				},
+				{
+					DNSName:    n.nsServerName(),
+					RecordTTL:  ttl,
+					RecordType: "A",
+					Targets:    NSServerIPs,
+				},
+			},
+		},
+	}
+	res, err := n.ensureDNSEndpoint(k8gbNamespace, NSRecord)
+	if err != nil {
+		return res, err
+	}
+	return nil, nil
+}
+
+func (n *Ns1) Finalize() (err error) {
+	log.Info("Successfully finalized Gslb")
+	return
+}
+
+func (n *Ns1) nsServerNameExt() []string {
+	dnsZoneIntoNS := strings.ReplaceAll(n.config.DNSZone, ".", "-")
+	var extNSServers []string
+	for _, clusterGeoTag := range n.config.ExtClustersGeoTags {
+		extNSServers = append(extNSServers,
+			fmt.Sprintf("gslb-ns-%s-%s.%s",
+				dnsZoneIntoNS,
+				clusterGeoTag,
+				n.config.EdgeDNSZone))
+	}
+	return extNSServers
+}
+
+func (n *Ns1) nsServerName() string {
+	dnsZoneIntoNS := strings.ReplaceAll(n.config.DNSZone, ".", "-")
+	return fmt.Sprintf("gslb-ns-%s-%s.%s",
+		dnsZoneIntoNS,
+		n.config.ClusterGeoTag,
+		n.config.EdgeDNSZone)
+}
+
+func (n *Ns1) coreDNSExposedIPs() ([]string, error) {
+	coreDNSService := &corev1.Service{}
+
+	err := n.client.Get(context.TODO(), types.NamespacedName{Namespace: k8gbNamespace, Name: coreDNSExtServiceName}, coreDNSService)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Info(fmt.Sprintf("Can't find %s service", coreDNSExtServiceName))
+		}
+		return nil, err
+	}
+	var lbHostname string
+	if len(coreDNSService.Status.LoadBalancer.Ingress) > 0 {
+		lbHostname = coreDNSService.Status.LoadBalancer.Ingress[0].Hostname
+	} else {
+		errMessage := fmt.Sprintf("no Ingress LoadBalancer entries found for %s serice", coreDNSExtServiceName)
+		log.Info(errMessage)
+		err := coreerrors.New(errMessage)
+		return nil, err
+	}
+	IPs, err := utils.Dig(n.config.EdgeDNSServer, lbHostname)
+	if err != nil {
+		log.Info(fmt.Sprintf("Can't dig k8gb-coredns-lb service loadbalancer fqdn %s", lbHostname))
+		return nil, err
+	}
+	return IPs, nil
+}
+
+func (n *Ns1) ensureDNSEndpoint(namespace string, i *externaldns.DNSEndpoint) (*reconcile.Result, error) {
+	found := &externaldns.DNSEndpoint{}
+	err := n.client.Get(context.TODO(), types.NamespacedName{
+		Name:      i.Name,
+		Namespace: namespace,
+	}, found)
+	if err != nil && errors.IsNotFound(err) {
+
+		// Create the DNSEndpoint
+		log.Info(fmt.Sprintf("Creating a new DNSEndpoint:\n %s", prettyPrint(i)))
+		err = n.client.Create(context.TODO(), i)
+
+		if err != nil {
+			// Creation failed
+			log.Error(err, "Failed to create new DNSEndpoint", "DNSEndpoint.Namespace", i.Namespace, "DNSEndpoint.Name", i.Name)
+			return &reconcile.Result{}, err
+		}
+		// Creation was successful
+		return nil, nil
+	} else if err != nil {
+		// Error that isn't due to the service not existing
+		log.Error(err, "Failed to get DNSEndpoint")
+		return &reconcile.Result{}, err
+	}
+
+	// Update existing object with new spec
+	found.Spec = i.Spec
+	err = n.client.Update(context.TODO(), found)
+
+	if err != nil {
+		// Update failed
+		log.Error(err, "Failed to update DNSEndpoint", "DNSEndpoint.Namespace", found.Namespace, "DNSEndpoint.Name", found.Name)
+		return &reconcile.Result{}, err
+	}
+	return nil, nil
+}
+
+func prettyPrint(s interface{}) string {
+	prettyStruct, err := json.MarshalIndent(s, "", "\t")
+	if err != nil {
+		fmt.Println("can't convert struct to json")
+	}
+	return string(prettyStruct)
+}
